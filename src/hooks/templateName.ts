e9700caa25766cc518baf181152ac34632e33528
ArchAngel776 <karol.marciniak155@gmail.com>
2025-11-18 23:45:23 +0100

    Add capitalize-splitting hook function.

---
+++ src/hooks/templateName.ts
@@ -0,0 +1,4 @@
+export default function templateName(name: string): string
+{
+    return name.replace(/[A-Za-z0-9]+$/, match => match.replaceAll(/([A-Z])/g, "_$1").substring(1).toLocaleLowerCase())
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Migration from "@app/Migration"
 import Connection from "@app/Connection"
 import Transaction from "@app/Transaction"
+import templateName from "@hooks/templateName"
 
 
-export { Migration, Connection, Transaction }
+export { Migration, Connection, Transaction, templateName }
